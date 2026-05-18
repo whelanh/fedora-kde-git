b50d85b3cf94997087f95501de93f80a1bf6b0c2
Hadi Chokr <hadichokr@icloud.com>
2026-05-18 10:15:16 +0200

    Pull runtime deps too.

--- build_files/install-kde-deps.py
+++ build_files/install-kde-deps.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 # SPDX-FileCopyrightText: 2026 Hadi Chokr <hadichokr@icloud.com>
+
 import subprocess
 import urllib.request
 import yaml
@@ -11,15 +12,19 @@ KDE_DEPS_YAML = "https://invent.kde.org/sysadmin/repo-metadata/-/raw/master/dist
 def load_deps_from_yaml(url):
     with urllib.request.urlopen(url) as f:
         data = yaml.full_load(f)
+
     deps = []
     for pkg in data.values():
         deps.extend(pkg.get("builddeps", []))
+        deps.extend(pkg.get("rundeps", []))
+
     return list(set(deps))
 
 
 def install(packages):
     packages = list(set(packages))
     print(f"Installing {len(packages)} packages via dnf5")
+
     process = subprocess.run(
         [
             "dnf5", "install", "-y",
@@ -29,6 +34,7 @@ def install(packages):
             "--skip-broken",
         ] + packages,
     )
+
     if process.returncode != 0:
         raise Exception(f"dnf5 install failed ({process.returncode})")
 
